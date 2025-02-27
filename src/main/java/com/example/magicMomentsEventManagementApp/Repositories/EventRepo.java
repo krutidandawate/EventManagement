6dcaae0b05bf899e62e7d764d2b0e7632a5142bb
goelnishtha <123407441+goelnishtha@users.noreply.github.com>
2025-02-28 02:41:02 +0530

    EventRepo.java

--- src/main/java/com/example/magicMomentsEventManagementApp/Repositories/EventRepo.java
+++ src/main/java/com/example/magicMomentsEventManagementApp/Repositories/EventRepo.java
@@ -3,7 +3,7 @@ package com.example.magicMomentsEventManagementApp.Repositories;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import com.example.magicMoments.Models.EventModel;
+import com.example.magicMomentsEventManagementApp.Models.EventModel;
 
 public interface EventRepo extends JpaRepository<EventModel, Long> {
     @Query("SELECT e FROM EventModel e WHERE e.userId = ?1")
