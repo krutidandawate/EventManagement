bdda7bd26a80aebe4b10b3a7b9df031b868d3268
goelnishtha <123407441+goelnishtha@users.noreply.github.com>
2025-02-28 02:42:17 +0530

    UserRepo.java

--- src/main/java/com/example/magicMomentsEventManagementApp/Repositories/UserRepo.java
+++ src/main/java/com/example/magicMomentsEventManagementApp/Repositories/UserRepo.java
@@ -2,7 +2,7 @@ package com.example.magicMomentsEventManagementApp.Repositories;
 
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.magicMoments.Models.UserModel;
+import com.example.magicMomentEventManagementApps.Models.UserModel;
 
 public interface UserRepo extends JpaRepository<UserModel, Long> {
     List<UserModel> findByUsernameAndPassword(String username, String password);
