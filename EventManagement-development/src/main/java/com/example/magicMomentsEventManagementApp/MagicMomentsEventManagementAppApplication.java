25a2d05af8976a1abad704a3ba222a233d027c05
Abhilash291023 <129185525+Abhilash291023@users.noreply.github.com>
2025-02-28 11:11:43 +0530

    Add files via upload

---
+++ EventManagement-development/src/main/java/com/example/magicMomentsEventManagementApp/MagicMomentsEventManagementAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.magicMomentsEventManagementApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagicMomentsEventManagementAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagicMomentsEventManagementAppApplication.class, args);
+	}
+
+}
