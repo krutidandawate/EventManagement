367c38bdd5d4a1583e123ad524728e73bde42434
goelnishtha <123407441+goelnishtha@users.noreply.github.com>
2025-02-28 13:39:11 +0530

    Update UserRepo.java

--- EventManagement-development/src/main/java/com/example/magicMomentsEventManagementApp/Repositories/UserRepo.java
+++ EventManagement-development/src/main/java/com/example/magicMomentsEventManagementApp/Repositories/UserRepo.java
@@ -6,4 +6,8 @@ import com.example.magicMomentsEventManagementApp.Models.UserModel;
 
 public interface UserRepo extends JpaRepository<UserModel, Long> {
     List<UserModel> findByUsernameAndPassword(String username, String password);
+
+    
+    @Query(value= "SELECT `user_id`, `password`, `user_email`, `user_ph_no`, `username` FROM `user_model` WHERE `username`=?1 and `password`= ?2", nativeQuery = true)
+    List<UserModel> Validation(String username, String password);
 }
